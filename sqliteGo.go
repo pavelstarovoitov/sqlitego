04cc1194723fd29e090913d723c5c96aabb03da9
pavelstarovoitov <pavelstarovoitov91@gmail.com>
2025-12-23 17:21:24 +0300

    use mactsouk

--- sqliteGo.go
+++ sqliteGo.go
@@ -6,9 +6,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/mactsouk/sqlite06"
 	//"github.com/mactsouk/sqlite06"
-	//"github.com/mactsouk/sqlite06"
-	"github.com/pavelstarovoitov/sqlite06"
+	//"github.com/pavelstarovoitov/sqlite06"
 )
 
 var MIN = 0
